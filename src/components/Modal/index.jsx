25034c45a57832660536b9099b646d2362afed11
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2023-09-16 00:55:51 +0300

    class-modal

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,11 +1,16 @@
+import { Component } from 'react';
 import { ModalWindow, Overlay, ModalImg } from './Modal.styled';
 
-export default function Modal({ img }) {
-  return (
-    <Overlay>
-      <ModalWindow>
-        <ModalImg src={img} alt="" />
-      </ModalWindow>
-    </Overlay>
-  );
+export default class Modal extends Component {
+  render() {
+    const { img } = this.props;
+
+    return (
+      <Overlay>
+        <ModalWindow>
+          <ModalImg src={img} alt="" />
+        </ModalWindow>
+      </Overlay>
+    );
+  }
 }
